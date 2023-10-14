43dea93d6fb4c63558b9e63aba7a221d74a15cc8
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-10-14 20:58:50 +0330

    Initial commit

---
+++ src/main/java/ir/imorate/springbootfilepond/SpringBootFilepondApplication.java
@@ -0,0 +1,13 @@
+package ir.imorate.springbootfilepond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFilepondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFilepondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/imorate/springbootfilepond/SpringBootFilepondApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.imorate.springbootfilepond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFilepondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
